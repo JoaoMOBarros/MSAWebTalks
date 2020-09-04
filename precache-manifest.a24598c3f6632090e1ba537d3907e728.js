c955b39412a61aecb5004cf7aa0573028e3211d0
Joao Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-09-03 21:07:34 -0300

    Updates

---
+++ precache-manifest.a24598c3f6632090e1ba537d3907e728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83abe893d7148c8bf25fd61a7f40d23f",
+    "url": "/MSAWebTalks/index.html"
+  },
+  {
+    "revision": "f33d71141c8546923cdb",
+    "url": "/MSAWebTalks/static/css/main.29e3f8cf.chunk.css"
+  },
+  {
+    "revision": "25be7df7661985e8d4d8",
+    "url": "/MSAWebTalks/static/js/2.fe60f41d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MSAWebTalks/static/js/2.fe60f41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33d71141c8546923cdb",
+    "url": "/MSAWebTalks/static/js/main.6352cf95.chunk.js"
+  },
+  {
+    "revision": "524a1ed4676238778f41",
+    "url": "/MSAWebTalks/static/js/runtime-main.ab97efc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MSAWebTalks/static/media/logo.5d5d9eef.svg"
+  }
+]);
